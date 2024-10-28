b8046e395841412bf4dca315a85774c9c94da992
Abdullah Al Mamun <101423827+hummerheads@users.noreply.github.com>
2024-10-28 17:52:08 +0600

    Product Details Fixed

--- src/pages/products/Details/ProductDetails.jsx
+++ src/pages/products/Details/ProductDetails.jsx
@@ -13,14 +13,16 @@ const ProductDetails = () => {
   const [selectedId, setSelectedId] = useState(null);
 
   const { user } = useContext(AuthContext);
-  const { addToCart } = useContext(CartContext); 
+  const { addToCart } = useContext(CartContext);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://mediore-medicine-server.vercel.app/allmeds/');
+        const response = await fetch(
+          "https://mediore-medicine-server.vercel.app/allmeds/"
+        );
         const data = await response.json();
         setAllMeds(data);
       } catch (error) {
@@ -33,13 +35,13 @@ const ProductDetails = () => {
 
   useEffect(() => {
     if (allMeds.length > 0) {
-      const foundProduct = allMeds.find(med => med.id === parseInt(id));
+      const foundProduct = allMeds.find((med) => med.id === parseInt(id));
       setProduct(foundProduct);
     }
   }, [allMeds, id]);
 
   const toggleDropdown = () => {
-    setDropdownOpen(prev => !prev);
+    setDropdownOpen((prev) => !prev);
   };
 
   const handleSizeSelect = (unit) => {
@@ -64,21 +66,24 @@ const ProductDetails = () => {
       selectedId,
       img: product.medicine_image,
       category: product.category_name,
-      manufacturer: product.manufacturer_name
+      manufacturer: product.manufacturer_name,
     };
 
     try {
-      const response = await fetch("https://mediore-medicine-server.vercel.app/cart", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(cartItem),
-      });
+      const response = await fetch(
+        "https://mediore-medicine-server.vercel.app/cart",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(cartItem),
+        }
+      );
 
       if (response.ok) {
         console.log("Product added to cart successfully");
-        addToCart(cartItem); 
+        addToCart(cartItem);
       } else {
         console.error("Failed to add product to cart");
       }
@@ -90,47 +95,63 @@ const ProductDetails = () => {
   return (
     <div className="md:mx-20 my-5 md:my-10 mx-4">
       {product ? (
-        <div className="md:flex md:gap-10 justify-center my-10">
-          <div className="my-10">
+        <div className="md:flex md:gap-10 justify-center my-10 h-[500px]">
+          <div className="my-10 md:my-0 md:w-1/2">
             <img
-              className="w-auto mx-auto h-[50vh] shadow-2xl rounded-xl"
+              className="md:w-auto md:h-full w-auto mx-auto h-[50vh] shadow-md rounded-md"
               src={product.medicine_image || "/default-image.png"}
               alt={product.medicine_name}
             />
           </div>
-          <div className="md:w-1/2 py-5  text-white md:space-y-3 md:p-10 shadow-2xl rounded-xl bg-[#28A745] space-y-4 px-5">
-            <h2 className="md:text-5xl font-bold text-2xl">
+          <div className="md:w-1/2 py-5 md:px-20  shadow-md text-gray-700 rounded-md md:space-y-0  space-y-4 px-5">
+            <h2 className="md:text-5xl text-[#28A745] text-center md:py-10 font-bold text-2xl">
               {product.medicine_name}
-              <span className="font-normal md:text-xl text-xs"> {product.strength}</span>
-            </h2>
-            <h2 className="md:text-xl text-md"><span className="font-semibold">Category:</span> {product.category_name}</h2>
-            <h2 className="md:text-xl text-md"><span className="font-semibold">Generic:</span>{product.generic_name}</h2>
-            <h2 className="md:text-xl text-md"><span className="font-semibold">Manufacturer:</span>{product.manufacturer_name}</h2>
-            <h2 className="md:text-xl text-md">
-              Price:
-              <span className="font-bold md:text-2xl "> 
-                ${selectedPrice !== null ? selectedPrice : product.discount_value}
+              <span className="text-black font-normal md:text-xl text-xs">
+                {product.strength}
               </span>
             </h2>
+            <div className="md:space-y-5">
+              <h2 className="md:text-xl text-md">
+                <span className="font-semibold">Category:</span>{" "}
+                {product.category_name}
+              </h2>
+              <h2 className="md:text-xl text-md">
+                <span className="font-semibold">Generic: </span>
+                {product.generic_name}
+              </h2>
+              <h2 className="md:text-xl text-md">
+                <span className="font-semibold">Manufacturer: </span>
+                {product.manufacturer_name}
+              </h2>
+              <h2 className="md:text-xl text-md md:pb-5">
+                <span className="font-semibold">Price: </span>
+                <span className=" md:text-xl md:pb-5">
+                  $
+                  {selectedPrice !== null
+                    ? selectedPrice
+                    : product.discount_value}
+                </span>
+              </h2>
+            </div>
 
             <div>
               <button
                 onClick={toggleDropdown}
-                className="bg-white text-black rounded-md text-xs p-2 md:p-2 hover:bg-gray-500 hover:text-white md:text-xl"
+                className="mb-5 text-white bg-green-600 rounded-md text-xs p-2 md:p-2 hover:bg-green-800 hover:text-white md:text-xl"
               >
                 {selectedSize || "Select Size"}
               </button>
               {dropdownOpen && (
-                <div className="absolute ml-16 mx-10 md:mx-16 md:ml-52 bg-white text-black rounded-md shadow-lg mt-1 md:text-xl text-xs">
+                <div className="absolute md:px-5 ml-16 mx-10 md:text-left md:mx-0 bg-white text-black rounded-md shadow-lg mt-1 md:text-xl text-xs">
                   {product.unit_prices && product.unit_prices.length > 0 ? (
                     product.unit_prices.map((unit) => (
                       <div
                         key={unit.id}
-                        className="p-2 hover:bg-gray-200 cursor-pointer md:text-xl"
+                        className="p-2 hover:bg-gray-200 md:py-4 cursor-pointer md:text-xl"
                         onClick={() => handleSizeSelect(unit)}
                       >
                         {unit.unit_size} ml - {unit.unit} - ${unit.price}
-                      <hr />
+                        <hr />
                       </div>
                     ))
                   ) : (
@@ -143,7 +164,7 @@ const ProductDetails = () => {
             {selectedSize && (
               <button
                 onClick={handleAddtoCart}
-                className="mt-4 md:text-xl text-xs bg-white text-black rounded-md p-2 hover:bg-gray-500 hover:text-white"
+                className="mt-4  border-green-500 border-2 md:text-xl text-xs bg-white text-black rounded-md p-2 hover:bg-green-700 hover:border-none hover:duration-300 hover:text-white"
               >
                 Add to Cart
               </button>
@@ -153,12 +174,12 @@ const ProductDetails = () => {
       ) : (
         <p>Loading...</p>
       )}
-      <div className="mt-20 text-center my-5 md:my-10">
+      <div className="mt-20 text-center my-5 md:my-20">
         <Link
-          className=" p-2 font-bold rounded-xl bg-[#28A745] text-white hover:bg-green-700 text-xs md:text-2xl"
+          className=" p-2 md:px-4 rounded-md bg-[#28A745] text-white hover:bg-green-700 text-xs md:text-xl"
           to={"/product"}
         >
-          Go to All Products
+          All Products
         </Link>
       </div>
     </div>
